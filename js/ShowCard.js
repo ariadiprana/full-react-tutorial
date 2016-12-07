777906302633fc8e27078bb228da03e0e0b1975a
ariadiprana <ari.adiprana@gmail.com>
2016-12-08 00:55:10 +0700

    add search method

--- js/ShowCard.js
+++ js/ShowCard.js
@@ -1,22 +1,21 @@
 import React from 'react'
 
-const { shape, string } = React.PropTypes
+const { string } = React.PropTypes
 const ShowCard = React.createClass({
   propTypes: {
-    show: shape({
-      poster: string,
-      title: string,
-      year: string,
-      description: string
-    })
+    poster: string,
+    title: string,
+    year: string,
+    description: string,
+    imdbID: string
   },
   render () {
-    const { poster, title, year, description } = this.props.show
+    const { poster, title, year, description } = this.props
     return (
       <div className='show-card'>
         <img src={`/public/img/posters/${poster}`} />
         <div>
-          <h3>{title}</h3>
+          <h3>{title }</h3>
           <h4>{year}</h4>
           <p>{description}</p>
         </div>
